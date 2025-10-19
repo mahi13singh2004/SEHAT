0c7ef13176cd5d3efa17fe748a4811efd909fa55
mahi13singh2004 <mahi13singh2004@gmail.com>
2025-10-20 00:24:27 +0530

    added spinner

--- frontend/src/components/RestrictedRoute.jsx
+++ frontend/src/components/RestrictedRoute.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useAuthStore } from '../store/auth.store.js'
 import { Navigate } from 'react-router-dom'
+import Spinner from './Spinner'
 
 const RestrictedRoute = ({ children }) => {
   const { user, loading, checkAuth } = useAuthStore()
@@ -11,7 +12,11 @@ const RestrictedRoute = ({ children }) => {
     }
   }, [user, checkAuth]);
 
-  if (loading) return <p>....Loading</p>
+  if (loading) return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-950 via-slate-900 to-slate-800">
+      <Spinner size={64} />
+    </div>
+  )
   if (user) return <Navigate to="/" />
   return children
 }
